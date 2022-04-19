edea4c555de113b698594690a15a8d7e17215ad7
fuyoo <fuyoo@fuyoo.cc>
2022-04-19 18:10:51 +0800

    v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # vite-plugin-inject-wasm
 vite@2  plugin to inject wasm!
+
+## how to use
+
+1. **install**
+
+```hash
+pnpm install @fuyoo/vite-plugin-inject-wasm
+```
+
+2. **config plugin at the vite.config.ts**
+
+```js
+import {defineConfig} from 'vite'
+import {VitePluginInjectWasm} from '@fuyoo/vite-plugin-inject-wasm'
+
+export default defineConfig({
+    plugins: [vue(), VitePluginInjectWasm([{
+        name: "@fuyoo/wasm-hasher",
+        where: "pkg/wasm_hasher_bg.wasm" // **.wasm file at relate path for the package path
+    }]
+})
+```
+
+3. **use wasm package at other files `npm create vite@latest my-vue-app -- --template vue`**
+
+```vue
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import hasher from "@fuyoo/wasm-hasher"
+
+hasher().then(pkg => {
+  console.log(pkg)
+})
+</script>
+
+<template>
+
+</template>
+
+<style>
+</style>
+
+```
---
+++ src/main.ts
@@ -0,0 +1,83 @@
+import {copyFile} from "fs"
+import {join, basename, dirname} from "path"
+
+import dgram from "dgram";
+
+const ipv4 = async() => {
+    return new Promise<string>((resolve, reject) => {
+        if (Number(process.version.split(".")[0].replace('v','')) < 12) {
+            reject('node.js version must be >= 12.0.0')
+            return
+        }
+        const socket = dgram.createSocket('udp4')
+        socket.connect(80, '4.2.2.1', () => {
+            resolve(socket.address().address)
+            socket.close()
+        })
+        socket.on('error', e => resolve('127.0.0.1'))
+    })
+}
+
+
+// https://vitejs.dev/config/
+interface CfgItem {
+    name: string,
+    where: string
+}
+
+export const VitePluginInjectWasm = (cfg: Array<CfgItem>) => {
+    let host:string;
+    let server: any;
+    return {
+        name: "vite-plugin-inject-wasm",
+        configureServer(_server: any) {
+            if (server == null) {
+                server = _server
+            }
+        },
+        transform(code: string, id: string) {
+            try {
+                for (let item of cfg) {
+                    let v = new URL(`http://localohost/${id}`).searchParams.get("v")
+                    let pid = item.name.replace(/\//g, "_")
+                    if (id.indexOf(`${pid}.js`) > -1) {
+                        return new Promise<any>(async (resolve, reject) => {
+                            if (server != null) {
+                                if (host === "") {
+                                // get listen host
+                                host = server.config.server.host ? await ipv4() : "127.0.0.1"
+                                }
+                                // get package path
+                                let from_path = join(server.config.root, 'node_modules', item.name, item.where)
+                                // get save filename
+                                let to_filename = `${item.name}_${basename(item.where)}`.replace("/", "_")
+                                // get copy to file path
+                                let to_path = join(dirname(id), to_filename)
+                                // get code replace path from the cache dir
+                                let url_path = to_path.replace(server.config.root, "")
+                                // get server schema
+                                let schema = server.config.server.https ? "https" : "http"
+                                // change body
+                                console.log(basename(item.where))
+                                code = code.replace(basename(item.where),
+                                    `${schema}://${host}:${server.config.server.port}${url_path}?v=${v}`);
+                                // cp ***.wasm file to vite cache dir
+                                copyFile(from_path, to_path, (err: any) => {
+                                    if (err == null) {
+                                        resolve(code)
+                                    } else {
+                                        reject(err)
+                                    }
+                                })
+                            }
+                        })
+                    }
+                }
+            } catch (e) {
+                console.error(e)
+            }
+        }
+    }
+
+
+}
